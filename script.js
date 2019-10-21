1c72e99d65c919013c0a18bcea83e3946a8f082b
Gregory <gregorymarchand.pro@gmail.com>
2019-10-21 13:39:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const test = document.getElementById('container');
+
+function css(){
+   // test.style.color = 'red';
+    test.style.fontSize = '100px';
+    test.style.textDecoration = 'underline';
+}
+
+css();
